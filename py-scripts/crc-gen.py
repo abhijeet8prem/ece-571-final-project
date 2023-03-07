d11eeb3789310ed813af7047de0016a2cb0e57ed
Abhijeet Prem <abhij@pdx.edu>
2023-03-06 18:16:22 -0800

    script to generate crc

---
+++ py-scripts/crc-gen.py
@@ -0,0 +1,27 @@
+def crc_remainder(input_string, poly):
+    # Append zeros to the input string to make room for the CRC remainder
+    input_string += "0" * (len(poly) - 1)
+    
+    # Convert the input string and polynomial to binary integers
+    input_int = int(input_string, 2)
+    poly_int = int(poly, 2)
+    
+    # Calculate the CRC remainder using polynomial division
+    for i in range(len(input_string)):
+        if input_int >> (len(input_string) - i - 1) & 1:
+            input_int ^= poly_int << (len(input_string) - i - len(poly))
+    
+    # Convert the CRC remainder to a binary string
+    crc = bin(input_int)[2:]
+    
+    # Pad the CRC remainder with leading zeros if necessary
+    crc = "0" * (len(poly) - len(crc)) + crc
+    
+    return crc
+
+
+# Test the function
+input_string = "1101011011"
+poly = "1000100000010001"
+crc = crc_remainder(input_string, poly)
+print(crc)
