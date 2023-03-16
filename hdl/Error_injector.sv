add17d71643e8608a45474cf84546bc6cafa21a5
Abhijeet Prem <abhij@pdx.edu>
2023-03-16 14:05:20 -0700

    added interface connections

--- hdl/Error_injector.sv
+++ hdl/Error_injector.sv
@@ -1,6 +1,6 @@
 // vertion 2 code
 module error_injector( topInterface.errorInjectorV2 EIPorts);
 
- assign EIPorts.erCW = CW ^ erIn;   // adding the error with the incoming codeword
+ assign EIPorts.erCW <= EIPorts.CW ^ EIPorts.erIn;   // adding the error with the incoming codeword
  
 endmodule
