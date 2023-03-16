64ae1584a2258629c3b1f333944a26d6b75ea2af
habburi <125716858+habburi@users.noreply.github.com>
2023-03-16 12:24:01 -0700

    I added interface to connect the two modules

--- crcgen.sv
+++ crcgen.sv
@@ -1,18 +1,18 @@
-module crcgen(input clk, rst, eom, [15:0] dataIn , output reg crcValid, eot, [15:0] CrcOut, [15:0] dataOut); 
+module crcgen(topinterface.CrcIntA gen); 
   reg [15:0] Q; 
   bit databit;
   reg [31:0] datawithzeroes; //data appended with zeroes
   enum logic [1:0]{Init, Load, Reset}State, NextState; //FSM with three states
-  reg busy, ld_en, cnt_en, done, clr; //busy, load_enable, count_enable, done, clear
+  reg ld_en, cnt_en, done, clr; //busy, load_enable, count_enable, done, clear
   
-  assign datawithzeroes = {dataIn,16'b0};
-  assign eot = (eom == 1);
+  assign datawithzeroes = {gen.dataIn,16'b0};
+  assign eot = (gen.eom == 1);
   
-  DownCounter D(clk, ld_en, datawithzeroes, cnt_en, databit, done);
+  DownCounter D(gen.clk, ld_en, datawithzeroes, cnt_en, databit, done);
   
-  always_ff @(posedge clk)
+  always_ff @(posedge gen.clk)
     begin
-      if (rst)
+      if (gen.rst)
         State <= Init;
       else
         State <= NextState;
@@ -20,20 +20,20 @@ module crcgen(input clk, rst, eom, [15:0] dataIn , output reg crcValid, eot, [15
 
   always_comb
     begin
-      {busy, ld_en, cnt_en, crcValid, clr} = '0;
+      {gen.busy, ld_en, cnt_en, gen.crcValid, clr} = '0;
       case(State)
         Init: begin
-          busy = '0;
+          gen.busy = '0;
           ld_en = '1;
         end
         Load:begin
-          busy = '1;
+          gen.busy = '1;
           cnt_en = '1;
         end
         Reset:begin
           clr = '1;
-          busy = '1;
-	  crcValid = '1;
+          gen.busy = '1;
+	  gen.crcValid = '1;
         end
       endcase
     end
@@ -55,34 +55,34 @@ module crcgen(input clk, rst, eom, [15:0] dataIn , output reg crcValid, eot, [15
       endcase
     end
   
-  always_ff@(posedge clk)
+  always_ff@(posedge gen.clk)
     begin
       if(done == 1) begin
-         CrcOut <= Q;
-         dataOut <= dataIn;
+         gen.CrcOut <= Q;
+         gen.dataOut <= gen.dataIn;
       end
       else begin
-        CrcOut <= '0;
-        dataOut <= '0;
+        gen.CrcOut <= '0;
+        gen.dataOut <= '0;
       end
     end
 
-  DFF D0((databit^Q[15]),clk,rst,clr,Q[0]);
-  DFF D1(Q[0],clk,rst,clr,Q[1]);
-  DFF D2(Q[1],clk,rst,clr,Q[2]);
-  DFF D3(Q[2],clk,rst,clr,Q[3]);
-  DFF D4(Q[3],clk,rst,clr,Q[4]);
-  DFF D5((Q[4]^Q[15]),clk,rst,clr,Q[5]);
-  DFF D6(Q[5],clk,rst,clr,Q[6]);
-  DFF D7(Q[6],clk,rst,clr,Q[7]);
-  DFF D8(Q[7],clk,rst,clr,Q[8]);
-  DFF D9(Q[8],clk,rst,clr,Q[9]);
-  DFF D10(Q[9],clk,rst,clr,Q[10]);
-  DFF D11(Q[10],clk,rst,clr,Q[11]);
-  DFF D12((Q[11]^Q[15]),clk,rst,clr,Q[12]);
-  DFF D13(Q[12],clk,rst,clr,Q[13]);
-  DFF D14(Q[13],clk,rst,clr,Q[14]);
-  DFF D15(Q[14],clk,rst,clr,Q[15]);
+  DFF D0((databit^Q[15]),gen.clk,gen.rst,clr,Q[0]);
+  DFF D1(Q[0],gen.clk,gen.rst,clr,Q[1]);
+  DFF D2(Q[1],gen.clk,gen.rst,clr,Q[2]);
+  DFF D3(Q[2],gen.clk,gen.rst,clr,Q[3]);
+  DFF D4(Q[3],gen.clk,gen.rst,clr,Q[4]);
+  DFF D5((Q[4]^Q[15]),gen.clk,gen.rst,clr,Q[5]);
+  DFF D6(Q[5],gen.clk,gen.rst,clr,Q[6]);
+  DFF D7(Q[6],gen.clk,gen.rst,clr,Q[7]);
+  DFF D8(Q[7],gen.clk,gen.rst,clr,Q[8]);
+  DFF D9(Q[8],gen.clk,gen.rst,clr,Q[9]);
+  DFF D10(Q[9],gen.clk,gen.rst,clr,Q[10]);
+  DFF D11(Q[10],gen.clk,gen.rst,clr,Q[11]);
+  DFF D12((Q[11]^Q[15]),gen.clk,gen.rst,clr,Q[12]);
+  DFF D13(Q[12],gen.clk,gen.rst,clr,Q[13]);
+  DFF D14(Q[13],gen.clk,gen.rst,clr,Q[14]);
+  DFF D15(Q[14],gen.clk,gen.rst,clr,Q[15]);
 
 endmodule
 
--- top.sv
+++ top.sv
@@ -3,12 +3,12 @@ module top();
   bit clk = 1'b1;
   logic rst, crcValid, eom, eot;
   logic [7:0] m1, m2, ascii;
-  string s = "HARSHA ";
+  string s = "HARSHA";
   logic [7:0] msg[$];
-  int length, bit_flips;
+  int length;//, bit_flips;
   logic [31:0] error_bits; 
   
-  crcgen DUT(.*);
+  //crcgen DUT(.*);
   
   initial begin
     forever #5 clk = ~clk;
@@ -34,24 +34,25 @@ module top();
   
   task Evenlen(input int length, input logic [7:0] msg [$]);
     forever begin
-      if(~(top.DUT.busy)) begin
+      if(~(TI.busy)) begin
         if(msg.size() == 0)begin
 	  eom = '1;
-	  repeat(2)@(negedge clk);
+	  @(negedge clk);
           break;
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
-          bit_flips = $urandom_range(0,3);
-          Error_injection(bit_flips);
+	  assert(ER.randomize());
+          //bit_flips = $urandom_range(0,3);
+          //Error_injection(bit_flips);
         end
       end
-      
+      /*
       if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %d, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, bit_flips, eom, crcValid);
-      end
+         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
+      end*/
       @(negedge clk);
     end
     $finish();
@@ -59,30 +60,30 @@ module top();
       
   task Oddlen(input int length, input logic [7:0] msg [$]);
     forever begin
-      if(~(top.DUT.busy)) begin
+      if(~(TI.busy)) begin
         if(msg.size() == 0)begin
 	  eom = '1;
-	  @(negedge clk);
+          @(negedge clk);
           break;
 	end
         else if(msg.size() == 1)begin
           m1 = msg.pop_front();
           m2 = 8'b0;
           dataIn = {m1,m2};
-          bit_flips = $urandom_range(0,3);
-          Error_injection(bit_flips);
+          //bit_flips = $urandom_range(0,3);
+          //Error_injection(bit_flips);
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
-          bit_flips = $urandom_range(0,3);
-          Error_injection(bit_flips);
+          //bit_flips = $urandom_range(0,3);
+          //Error_injection(bit_flips);
         end
-      end
+      end/*
       if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %d, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, bit_flips, eom, crcValid);
-      end
+         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
+      end*/
       @(negedge clk);
     end
     $finish();
@@ -97,19 +98,54 @@ module top();
     error_bits = error_out;
   endtask
 
+  class Error_ratio;
+    rand int bit_flips;
+    constraint c_flips {
+      bit_flips dist {0 := 50, [1:2] := 40, [3:6] := 10};}
+  endclass
+
+  topinterface TI(.*);
+    
+  crcgen geneartor(TI.CrcIntA);
+
+  Error_ratio ER;      
   initial begin
-    rst = 1'b1;
-    @(negedge clk);
-    rst = 1'b0;
+    reset();
     length = s.len();
     $display("%d",length);
     strtoascii(length,s);
+    ER = new();
     
     if(length%2 == 0) 
-        Evenlen(length, msg);
+      Evenlen(length, msg);
     else 
       Oddlen(length, msg);
+
+    forever begin
+      if(~(TI.busy)) begin
+        if(msg.size() == 0)
+          break;
+        else begin
+          assert(ER.randomize()) $info("random error success %d",ER.bit_flips);
+          Error_injection(ER.bit_flips);
+        end
+      end
+    end
+    
   end
 endmodule
 
+interface topinterface(input logic clk, rst, [15:0] dataIn);
+  logic [15:0] dataOut;
+  logic [15:0]CrcOut, CrcRem;
+  logic crcValid, tag, eom, eot, data_er, busy;
+  
+  modport CrcIntA(input clk, rst, dataIn, eom, output crcValid, eot, busy, CrcOut, dataOut);
+  
+ // modport ErrInj(input dataOut, CrcOut, crcValid, eot, output data_er, Crc_er, eof); 
+  
+  //modport CrcIntB(input clock, rst, data_er, Crc_er, eof, output crcValid, CrcOut, CrcRem);
+  
+  //modport PatInt(input CrcRem, output tag, iserror, CorVec, Corvec4);
+endinterface
 
