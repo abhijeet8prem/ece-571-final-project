414c1042983b4b19cfe811c3841813942e72b875
habburi <125716858+habburi@users.noreply.github.com>
2023-03-19 10:46:02 -0700

    Add files via upload

--- top.sv
+++ top.sv
@@ -1,15 +1,45 @@
 module top();
+  
+  parameter file = "input-message.txt";
+  
   logic [15:0] dataIn, CrcOut, dataOut;
   bit clk = 1'b1;
-  logic rst, crcValid, eom, eot;
+  logic rst, dataValid;
   logic [7:0] m1, m2, ascii;
-  string s = "HARSHA";
+  //string s = "HARSHA";
+  string fileContents;                       // String variable to store the input message
   logic [7:0] msg[$];
-  int length;//, bit_flips;
-  logic [31:0] error_bits; 
+  int length;            // input error vector for the error injuctor module
+  logic endMsgIn; 
+  logic [31:0] erIn = {2'b11,{30{1'b0}}};                 // singal to indicate end of message transmission
   
-  //crcgen DUT(.*);
+  topInterface    TI(.*); 
+  crcTxPort       TX(TI.transmitter);
+  errorInj        EI(TI.errorInjector);
+  rx_wrapper      RX(TI.receiver);
   
+  // Initial block to perform file handeling
+  initial begin
+      // Open the file
+      automatic int file_handle = $fopen(file, "r");
+      automatic byte data;
+      
+      if (file_handle == 0) begin
+        $display("Error opening file %s", file);
+        $finish;
+      end
+
+      // Read the file contents into the string variable
+      
+      while (!$feof(file_handle)) begin
+        $fread(data, file_handle);
+        fileContents = {fileContents, $sformatf("%c", data)};
+      end
+
+      // Close the file
+      $fclose(file_handle);
+    end
+
   initial begin
     forever #5 clk = ~clk;
   end
@@ -32,11 +62,12 @@ module top();
     end
   endtask
   
-  task Evenlen(input int length, input logic [7:0] msg [$]);
+  task Evenlen(input int length, input logic [7:0] msg [$]);  
     forever begin
+      dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	  eom = '1;
+	  endMsgIn = '1;
 	  @(negedge clk);
           break;
         end
@@ -44,25 +75,22 @@ module top();
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
+          dataValid = '1;
 	  assert(ER.randomize());
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
         end
       end
-      /*
-      if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
-      end*/
       @(negedge clk);
     end
+    repeat(10)@(negedge clk);
     $finish();
   endtask
       
   task Oddlen(input int length, input logic [7:0] msg [$]);
     forever begin
+      dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	  eom = '1;
+	  endMsgIn = '1;
           @(negedge clk);
           break;
 	end
@@ -70,82 +98,38 @@ module top();
           m1 = msg.pop_front();
           m2 = 8'b0;
           dataIn = {m1,m2};
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
+          dataValid = '1;
+	  assert(ER.randomize());
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
+          dataValid = '1;
+	  assert(ER.randomize());
         end
-      end/*
-      if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
-      end*/
+      end
       @(negedge clk);
     end
     $finish();
   endtask
-          
-  task Error_injection(input int bit_flips);
-    bit [31:0] error_out;
-    error_out = '0;
-    for(int j = 0; j < bit_flips; j++) begin
-      error_out[$urandom_range(0,31)] = 1'b1;
-    end
-    error_bits = error_out;
-  endtask
 
   class Error_ratio;
-    rand int bit_flips;
-    constraint c_flips {
-      bit_flips dist {0 := 50, [1:2] := 40, [3:6] := 10};}
+    rand logic  [31:0] erIn1;
+    constraint c_flips { $countones(erIn1) == 3; }
   endclass
-
-  topinterface TI(.*);
-    
-  crcgen geneartor(TI.CrcIntA);
 
   Error_ratio ER;      
   initial begin
     reset();
-    length = s.len();
+    length = fileContents.len();
     $display("%d",length);
-    strtoascii(length,s);
+    strtoascii(length,fileContents);
     ER = new();
     
     if(length%2 == 0) 
       Evenlen(length, msg);
     else 
-      Oddlen(length, msg);
-
-    forever begin
-      if(~(TI.busy)) begin
-        if(msg.size() == 0)
-          break;
-        else begin
-          assert(ER.randomize()) $info("random error success %d",ER.bit_flips);
-          Error_injection(ER.bit_flips);
-        end
-      end
-    end
-    
+      Oddlen(length, msg);    
   end
 endmodule
-
-interface topinterface(input logic clk, rst, [15:0] dataIn);
-  logic [15:0] dataOut;
-  logic [15:0]CrcOut, CrcRem;
-  logic crcValid, tag, eom, eot, data_er, busy;
-  
-  modport CrcIntA(input clk, rst, dataIn, eom, output crcValid, eot, busy, CrcOut, dataOut);
-  
- // modport ErrInj(input dataOut, CrcOut, crcValid, eot, output data_er, Crc_er, eof); 
-  
-  //modport CrcIntB(input clock, rst, data_er, Crc_er, eof, output crcValid, CrcOut, CrcRem);
-  
-  //modport PatInt(input CrcRem, output tag, iserror, CorVec, Corvec4);
-endinterface
-
