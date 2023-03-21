af1640ae17bd706e6eab64e64a9c3b2b9c06352e
Abhijeet Prem <abhij@pdx.edu>
2023-03-20 21:59:38 -0700

    removing old files

--- hdl/Error_injector.sv
+++
@@ -1,6 +0,0 @@
-// vertion 2 code
-module error_injector( topInterface.errorInjectorV2 EIPorts);
-
- assign EIPorts.erCW <= EIPorts.CW ^ EIPorts.erIn;   // adding the error with the incoming codeword
- 
-endmodule
