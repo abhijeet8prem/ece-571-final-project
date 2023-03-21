d5a1ef506125c1da1906054845572ac43948e996
Abhijeet Prem <abhij@pdx.edu>
2023-03-21 14:40:52 -0700

    bug fix for passing error vectors

--- hdl/top.sv
+++ hdl/top.sv
@@ -1,24 +1,34 @@
+ 
+ class Error_ratio;
+    rand logic  [31:0] erIn;
+    constraint c_flips { $countones(erIn) == 1; }
+endclass
+
+
 module top();
   
   parameter file = "input-message.txt";
   
-  logic [15:0] dataIn, CrcOut, dataOut;
+  //logic [15:0] dataIn,;
   bit clk = 1'b1;
-  logic rst, dataValid;
+  logic rst;// dataValid;
   logic [7:0] m1, m2, ascii;
   //string s = "HARSHA";
   string fileContents;                       // String variable to store the input message
   logic [7:0] msg[$];
   int length;            // input error vector for the error injuctor module
-  logic endMsgIn; 
-  logic [31:0] erIn = {2'b11,{30{1'b0}}};                 // singal to indicate end of message transmission
+  //logic endMsgIn; 
+  //logic [31:0] erIn = {2'b11,{30{1'b0}}};                 // singal to indicate end of message transmission
   
+  Error_ratio ER = new();       // creating the object for the error class
+  //wire [31:0] erIn1 = ER.erIn1;
   topInterface    TI(.*); 
   crcTxPort       TX(TI.transmitter);
   errorInj        EI(TI.errorInjector);
   rx_wrapper      RX(TI.receiver);
 
-
+  
+  
   // Initial block to perform file handeling
   initial begin
       // Open the file
@@ -63,21 +73,22 @@ module top();
     end
   endtask
   
-  task Evenlen(input int length, input logic [7:0] msg [$]);  
+  task Evenlen(input int length, input logic [7:0] msg [$]); 
     forever begin
-      dataValid = '0;
+      TI.dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	  endMsgIn = '1;
-	  @(negedge clk);
+	        TI.endMsgIn = '1;
+	        @(negedge clk);
           break;
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
-          dataIn = {m1,m2};
-          dataValid = '1;
-	  assert(ER.randomize());
+          TI.dataIn = {m1,m2};
+          TI.dataValid = '1;
+	        assert(ER.randomize());
+          TI.erIn = ER.erIn;
         end
       end
       @(negedge clk);
@@ -85,29 +96,31 @@ module top();
     repeat(10)@(negedge clk);
     $finish();
   endtask
-      
+
   task Oddlen(input int length, input logic [7:0] msg [$]);
     forever begin
-      dataValid = '0;
+      TI.dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	  endMsgIn = '1;
+	        TI.endMsgIn = '1;
           @(negedge clk);
           break;
 	end
         else if(msg.size() == 1)begin
           m1 = msg.pop_front();
           m2 = 8'b0;
-          dataIn = {m1,m2};
-          dataValid = '1;
-	  assert(ER.randomize());
+          TI.dataIn = {m1,m2};
+          TI.dataValid = '1;
+	        assert(ER.randomize() );
+          TI.erIn = ER.erIn;
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
-          dataIn = {m1,m2};
-          dataValid = '1;
-	  assert(ER.randomize());
+          TI.dataIn = {m1,m2};
+          TI.dataValid = '1;
+	        assert(ER.randomize() );
+          TI.erIn = ER.erIn;
         end
       end
       @(negedge clk);
@@ -115,19 +128,12 @@ module top();
     $finish();
   endtask
 
-  class Error_ratio;
-    rand logic  [31:0] erIn1;
-    constraint c_flips { $countones(erIn1) == 3; }
-  endclass
-
-  Error_ratio ER;      
   initial begin
     reset();
     length = fileContents.len();
     $display("%d",length);
     strtoascii(length,fileContents);
-    ER = new();
-    
+      
     if(length%2 == 0) 
       Evenlen(length, msg);
     else 
