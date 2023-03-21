9ebc415913da09bce6cdcbe837f33d2d2f5cdf9f
Abhijeet Prem <abhij@pdx.edu>
2023-03-20 22:35:18 -0700

    working modules as of Sunday

--- hdl/top.sv
+++ hdl/top.sv
@@ -1,31 +1,25 @@
--// Enter the file desciption here
-
 module top();
-
+  
   parameter file = "input-message.txt";
   
   logic [15:0] dataIn, CrcOut, dataOut;
   bit clk = 1'b1;
-  logic rst, crcValid, endOfMsg, eot;
+  logic rst, dataValid;
   logic [7:0] m1, m2, ascii;
   //string s = "HARSHA";
   string fileContents;                       // String variable to store the input message
-  
   logic [7:0] msg[$];
-  int length;//, bit_flips;
-  logic [31:0] error_bits;
-
-  logic [31:0] erIn,              // input error vector for the error injuctor module
-  logic endMsgIn                  // singal to indicate end of message transmission
+  int length;            // input error vector for the error injuctor module
+  logic endMsgIn; 
+  logic [31:0] erIn = {2'b11,{30{1'b0}}};                 // singal to indicate end of message transmission
   
-  //crcgen DUT(.*);
   topInterface    TI(.*); 
-  crcgen          TX(TI.transimitter);
-  error_injector  EI(TI.errorInjectorV2);
+  crcTxPort       TX(TI.transmitter);
+  errorInj        EI(TI.errorInjector);
   rx_wrapper      RX(TI.receiver);
 
 
-// Initial block to perform file handeling
+  // Initial block to perform file handeling
   initial begin
       // Open the file
       automatic int file_handle = $fopen(file, "r");
@@ -47,12 +41,9 @@ module top();
       $fclose(file_handle);
     end
 
-  
   initial begin
     forever #5 clk = ~clk;
   end
-
-
   
   task reset();
     begin
@@ -72,37 +63,35 @@ module top();
     end
   endtask
   
-  task Evenlen(input int length, input logic [7:0] msg [$]);
+  task Evenlen(input int length, input logic [7:0] msg [$]);  
     forever begin
+      dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	        endOfMsg = '1;
-	        @(negedge clk);
+	  endMsgIn = '1;
+	  @(negedge clk);
           break;
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
+          dataValid = '1;
 	  assert(ER.randomize());
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
         end
       end
-      /*
-      if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
-      end*/
       @(negedge clk);
     end
+    repeat(10)@(negedge clk);
     $finish();
   endtask
       
   task Oddlen(input int length, input logic [7:0] msg [$]);
     forever begin
+      dataValid = '0;
       if(~(TI.busy)) begin
         if(msg.size() == 0)begin
-	  endOfMsg = '1;
+	  endMsgIn = '1;
           @(negedge clk);
           break;
 	end
@@ -110,41 +99,27 @@ module top();
           m1 = msg.pop_front();
           m2 = 8'b0;
           dataIn = {m1,m2};
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
+          dataValid = '1;
+	  assert(ER.randomize());
         end
         else begin
           m1 = msg.pop_front();
           m2 = msg.pop_front();
           dataIn = {m1,m2};
-          //bit_flips = $urandom_range(0,3);
-          //Error_injection(bit_flips);
+          dataValid = '1;
+	  assert(ER.randomize());
         end
-      end/*
-      if(top.DUT.clr == '1)begin
-         $display("%b, %b, %b, %b, %b, %b, %b, %b", m1, m2, dataIn, dataOut, CrcOut, error_bits, eom, crcValid);
-      end*/
+      end
       @(negedge clk);
     end
     $finish();
   endtask
-          
-  task Error_injection(input int bit_flips);
-    bit [31:0] error_out;
-    error_out = '0;
-    for(int j = 0; j < bit_flips; j++) begin
-      error_out[$urandom_range(0,31)] = 1'b1;
-    end
-    error_bits = error_out;
-  endtask
 
   class Error_ratio;
-    rand int bit_flips;
-    constraint c_flips {
-      bit_flips dist {0 := 50, [1:2] := 40, [3:6] := 10};}
+    rand logic  [31:0] erIn1;
+    constraint c_flips { $countones(erIn1) == 3; }
   endclass
 
-  
   Error_ratio ER;      
   initial begin
     reset();
@@ -156,18 +131,6 @@ module top();
     if(length%2 == 0) 
       Evenlen(length, msg);
     else 
-      Oddlen(length, msg);
-
-    forever begin
-      if(~(TI.busy)) begin
-        if(msg.size() == 0)
-          break;
-        else begin
-          assert(ER.randomize()) $info("random error success %d",ER.bit_flips);
-          Error_injection(ER.bit_flips);
-        end
-      end
-    end
-    
+      Oddlen(length, msg);    
   end
 endmodule
