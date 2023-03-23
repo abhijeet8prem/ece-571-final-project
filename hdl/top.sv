2b7c748b3fc5b2247149688df6bc97d756fa18e5
Abhijeet Prem <abhij@pdx.edu>
2023-03-23 01:03:39 -0700

    updated file handling
    * removed unwanted string conversions
    * single run task
    * simplified message generation
    * capable or generating output file

--- hdl/top.sv
+++ hdl/top.sv
@@ -7,47 +7,43 @@ endclass
 module top();
   
   parameter file = "input-message.txt";
-  
+  parameter o_file = "out_file.txt";
+  int input_file, output_file;
   bit clk = 1'b1;
-  logic rst;// dataValid;
-  logic [7:0] m1, m2, ascii;
-  string fileContents;    // String variable to store the input message
-  logic [7:0] msg[$];
-  logic [15:0] receivedOut[$];
-  int length;            // input error vector for the error injuctor module
+  logic rst;
+  logic [7:0] m1, m2;
 
+
+  //logic [15:0] receivedOut[$];
+  
+// instantiate all the modules
   
   Error_ratio ER = new();       // creating the object for the error class
-
   topInterface    TI(.*); 
   crcTxPort       TX(TI.transmitter);
   errorInj        EI(TI.errorInjector);
   rx_wrapper      RX(TI.receiver);
 
   
-  
-  // Initial block to perform file handeling
-  initial begin
-      // Open the file
-      automatic int file_handle = $fopen(file, "r");
-      automatic byte data;
-      if (file_handle == 0) begin
-        $display("Error opening file %s", file);
-        $finish;
-      end
-      // Read the file contents into the string variable   
-      while (!$feof(file_handle)) begin
-        $fread(data, file_handle);
-        fileContents = {fileContents, $sformatf("%c", data)};
-      end
-      // Close the file
-      $fclose(file_handle);
-    end
 
+// clock generator 
   initial begin
     forever #5 clk = ~clk;
   end
-  
+
+// check for error in opening file
+  initial
+  begin
+  input_file = $fopen(file, "r");
+  output_file = $fopen(o_file,"w");  
+  if (input_file == 0) 
+  begin
+      $display("Error: opening input file %s", file);
+      $finish;
+  end
+  end
+
+// task to reset the system
   task reset();
     begin
       rst = '1;
@@ -56,81 +52,78 @@ module top();
     end
   endtask
   
-  task strtoascii(input int length, input string s);
-    begin
-      for(int i = 0; i < length; i++)begin
-        ascii = s.getc(i);
-        msg = {msg,ascii};
-        $display($time,"%p",msg);
-      end
-    end
-  endtask
-  
-  task Evenlen(input int length, input logic [7:0] msg [$]); 
-    forever begin
-      TI.dataValid = '0;
-      if(~(TI.busy)) begin
-        if(msg.size() == 0)begin
-	   TI.endMsgIn = '1;
-	   @(negedge clk);
-          break;
-        end
-        else begin
-          m1 = msg.pop_front();
-          m2 = msg.pop_front();
-          TI.dataIn = {m1,m2};
-          TI.dataValid = '1;
-	  assert(ER.randomize());
-          TI.erIn = ER.erIn;
-        end
-      end
-      @(negedge clk);
-    end
-    repeat(100)@(negedge clk);
-    $finish();
-  endtask
+  // Task to read a file and send the message to the port
 
-  task Oddlen(input int length, input logic [7:0] msg [$]);
+  task run(); 
+    
     forever begin
-      TI.dataValid = '0;
-      if(~(TI.busy)) begin
-        if(msg.size() == 0)begin
-	  TI.endMsgIn = '1;
+
+    // trnasmittor side  
+    TI.dataValid = '0;                  // setting that data is not valid
+    TI.endMsgIn = '0;                   // setting to default value
+            
+    if(~(TI.busy))                      // checking if the TX is busy
+    begin
+      m1 = $fgetc(input_file);          // extracting the first character                               
+      if(m1 == 8'hff)                   // check if the end of file is reached
+      begin                             
+        TI.endMsgIn = '1;               // assert end of transmission
+        @(negedge clk);
+        break;                          // exit forever TX loop
+      end
+      else                              // if end of file is not reached
+      begin
+        m2 = $fgetc(input_file);        // reading second character
+        if(m2 == 8'hff)                 // checking if its the end of file
+        begin                           // if true, end the transmission,
+          TI.dataIn = {m1,m2};          // appeinding both char together and asserting it
+                                        // dataIn port
+          TI.dataValid = '1;            
+          TI.endMsgIn = '1;
+          assert(ER.randomize());       // generating random error
+          TI.erIn = ER.erIn;            // 
           @(negedge clk);
           break;
-	end
-        else if(msg.size() == 1)begin
-          m1 = msg.pop_front();
-          m2 = 8'b0;
+        end
+        else
+        begin 
           TI.dataIn = {m1,m2};
           TI.dataValid = '1;
-	  assert(ER.randomize());
+          assert(ER.randomize());
           TI.erIn = ER.erIn;
-        end
-        else begin
-          m1 = msg.pop_front();
-          m2 = msg.pop_front();
-          TI.dataIn = {m1,m2};
-          TI.dataValid = '1;
-	  assert(ER.randomize() );
-          TI.erIn = ER.erIn;
-        end
+        end      
       end
-      @(negedge clk);
     end
-    $finish();
+	
+  // receiver side  
+	if(TI.dOutValid)                      // checking if valid codeword present
+    begin         
+      $monitor("in TI.dOutValid:%d MSG: %c%c \n",TI.dOutValid, TI.dOut[15:8],TI.dOut[7:0]);     
+      if(TI.endMsgOut)                   // check if the end of file is reached
+      begin
+        $fwrite(output_file,"%c%c",TI.dOut[15:8],TI.dOut[7:0]);                                 
+        @(negedge clk);
+        $fclose(output_file);             // close the file 
+                                          // exit forever TX loop
+      end
+      else                              // if end of file is not reached
+      begin
+       $fwrite(output_file,"%c%c",TI.dOut[15:8],TI.dOut[7:0]);
+      end
+    end
+
+    @(negedge clk);  
+    end
+    repeat(100)@(negedge clk);
+    $fclose(input_file);             // close the file
+    $finish();                       
   endtask
 
+  
   initial begin
     reset();
-    length = fileContents.len();
-    $display("%d",length);
-    strtoascii(length,fileContents);
+    run();
       
-    if(length%2 == 0) 
-      Evenlen(length, msg);
-    else 
-      Oddlen(length, msg);    
   end
 
 endmodule
